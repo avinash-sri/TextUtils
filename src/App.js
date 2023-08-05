417ed8554a58ab6d40c3612a12f61655c9591580
Avinash <avinashsrivastav76@gmail.com>
2023-08-05 19:59:50 +0530

    adding about

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import { useState } from 'react';
 import './App.css';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor='#1c3d6b'
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor='white'
+    }
+  }
+
   return (
     <>
       {/* <Navbar /> */}
-      <Navbar title="TextUtils" />
-      <TextForm heading="Enter the text to analyze" />
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <TextForm heading="Enter the text to analyze" mode={mode} />
+      <About />
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -19,24 +19,24 @@ export default function TextForm(props) {
         setText(event.target.value)
     }
 
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
     // setText('New text');
     return (
         <>
-            <div className='container my-3'>
+            <div className='container my-3' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="9"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{ background: props.mode === 'dark' ? '#1c3d6b' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} id="myBox" rows="9"></textarea>
                 </div>
                 <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
                 <button className="btn btn-warning mx-3" onClick={handleLoClick}>Convert to LowerCase</button>
             </div>
-            <div className="container">
+            <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>Your Text Summary</h1>
                 <p>{text.split(" ").length} words and {text.length} characters</p>
                 <p>Approximate minute to read the sentence is {0.008 * text.split(" ").length}</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length > 0 ? text : "Enter text in textbox above to preview here."}</p>
             </div>
         </>
     )
